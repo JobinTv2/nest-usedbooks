64284f2bdac3235d4f27d4e09e23ddd391a02881
Jobin T v <jobintv@focaloid.com>
2022-11-22 15:41:41 +0530

    feat: Test completed for order controller

--- src/order/order.controller.spec.ts
+++ src/order/order.controller.spec.ts
@@ -6,7 +6,24 @@ import { OrderService } from './order.service';
 
 describe('OrderController', () => {
   let controller: OrderController;
-  const orderMockService = {};
+  const orders = [
+    {
+      id: 1,
+      transaction_id: '1000',
+      user_id: 1,
+      book_id: 10,
+      address: 'address',
+    },
+  ];
+  const orderMockService = {
+    create: jest.fn((dto) => {
+      return { id: Date.now(), ...dto };
+    }),
+    update: jest.fn((id, dto) => {
+      const order = orders.find((ordr) => ordr.id === id);
+      return { ...order, ...dto };
+    }),
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OrderController],
@@ -21,5 +38,26 @@ describe('OrderController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create order', () => {
+    expect(
+      controller.create({
+        transaction_id: '1000',
+        user_id: 1,
+        book_id: 10,
+        address: 'address',
+      }),
+    ).toEqual({
+      transaction_id: '1000',
+      user_id: 1,
+      book_id: 10,
+      address: 'address',
+      id: expect.any(Number),
+    });
+  });
+
+  it('should update order', () => {
+    expect(controller.update('1', {})).toEqual(orders[0]);
   });
 });
